ce4ed00e2dab95ba2e36f24d8841cc1ccb31c6c4
CaioHPT <caio.h.teixeira@gmail.com>
2022-01-25 00:05:57 -0300

    alter iframe

--- src/components/Video/index.jsx
+++ src/components/Video/index.jsx
@@ -5,21 +5,21 @@ import { connect } from "react-redux";
 import './index.css'
 
 function Video({ activeLesson, activeModule }) {
-    if (activeLesson.title == "Primeira aula") {
+    if (activeLesson.title === "Primeira aula") {
         var Video = <iframe width="560" height="315" src="https://www.youtube.com/embed/7A4UQGrFU9Q" 
         title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; 
         picture-in-picture" ></iframe>
     }
-    else if (activeLesson.title == "Segunda aula") {
+    else if (activeLesson.title === "Segunda aula") {
         var Video = <iframe width="560" height="315" src="https://www.youtube.com/embed/XuhV0l3uugU" title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
     }
-    else if (activeLesson.title == "Terceira aula") {
+    else if (activeLesson.title === "Terceira aula") {
         var Video = <iframe width="560" height="315" src="https://www.youtube.com/embed/u99tNt3TZf8"
             title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; 
         encrypted-media; gyroscope; picture-in-picture"></iframe>
     }
-    else if (activeLesson.title == "Quarta aula") {
+    else if (activeLesson.title === "Quarta aula") {
         var Video = <iframe width="560" height="315" src="https://www.youtube.com/embed/6WB16wZS61c" title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
     } else {
